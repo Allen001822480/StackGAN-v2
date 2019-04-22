80a510236dc333427bf8e21f76450d566b87d484
Allen001822480 <35416660+Allen001822480@users.noreply.github.com>
2019-04-22 16:38:49 -0400

    Update trainer.py

--- code/trainer.py
+++ code/trainer.py
@@ -17,7 +17,7 @@ from miscc.config import cfg
 from miscc.utils import mkdir_p
 
 from tensorboard import summary
-from tensorboardX import FileWriter
+from tensorboardflow.summary import FileWriter
 
 from model import G_NET, D_NET64, D_NET128, D_NET256, D_NET512, D_NET1024, INCEPTION_V3
 
